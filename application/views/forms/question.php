c7a3ac37e75f5c004f781b985744ff742d3568ae
Basti25 <ich250@freenet.de>
2014-01-04 12:24:43 +0100

    - Frageneinfügen Formular hinzugefügt.

--- application/views/forms/question.php
+++ application/views/forms/question.php
@@ -2,7 +2,11 @@
 <?php include($config['rootpath'] . 'application/views/layout/header.php') ?>
     <div class="container">
         <?php
+        $level = $_POST['level'];
         $frage = $_POST['frage'];
+        $antwort = $_POST['antwort'];
+        $x = $_POST['x'];
+        $y = $_POST['y'];
         $send = false;
 
         if(isset($_POST['submit'])) {
@@ -10,7 +14,14 @@
                 echo 'Es muss eine Frage eingegeben werden.';
                 $send = true;
             }
-            if(!empty($frage)) {
+            if(empty($y) && empty($x)) {
+                echo 'Die Koordinaten sind nicht vollst&auml;ndig.';
+                $send = true;
+            }
+            if(!empty($frage) && !empty($y) && !empty($x)) {
+                $frage = '\'' . $frage . '\'';
+                $antwort = '\'' . $antwort . '\'';
+                $dbH->makeEntry($config['db_table_question'] . ' (level, question, answer , X, Y)', "($level, $frage, $antwort, $x, $y)");
                 echo 'Frage erfolgreich abgeschickt.';
             }
         } else {
@@ -19,10 +30,22 @@
         if($send): ?>
 
             <form method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
+                <label for="level">Schwierigkeitsgrad(Level) der Frage:</label>
+                <select id="level" name="level">
+                    <option value="1" selected>1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
                 <label for="frage">Frage:</label>
-                <textarea id="frage" name="frage">
+                <textarea id="frage" name="frage"></textarea>
+                <label for="antwort">Antwort (optional):</label>
+                <input type="text" id="antwort" name="antwort">
+                <label for="x">X Koordinate:</label>
+                <input type="text" id="x" name="x">
+                <label for="y">Y Koordinate:</label>
+                <input type="text" id="y" name="y">
 
-                </textarea>
                 <input type="submit" name="submit" value="senden"/>
             </form>
         <?php endif; ?>
